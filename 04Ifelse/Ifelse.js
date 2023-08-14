3d14a31ce0a0c7d5d1629edf09ac775936c1448e
Vinay-ambekar <vinayambekar.b@gmail.com>
2023-08-14 09:56:15 +0530

    'If-else'

---
+++ 04Ifelse/Ifelse.js
@@ -0,0 +1,11 @@
+let a=20
+if(a>18){
+    console.log('you can apply of driving ')
+}else if(a>=12 && a<=15){
+    console.log('you can lerne for driveing')
+}else{
+    console.log('you are not eligibel')
+}
+let marks=20
+let res=(marks>=10)?'you are pass':'not passed'
+console.log(res)
