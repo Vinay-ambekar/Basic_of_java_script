b912a4f0d2d82a3d7ab329513c48548a75a9babd
Vinay-ambekar <vinayambekar.b@gmail.com>
2023-08-15 12:15:58 +0530

    'array_methods'

--- 07array and there methods/12for__each.js
+++ 07array and there methods/12for__each.js
@@ -4,7 +4,7 @@ an array or the values of a map or set. It provides a
 convenient way to perform an operation 
 on each element/value without the need for manual indexing.*/
 //Iterating over an Array:
-const numbers08= [1, 2, 3, 4, 5]
+/*const numbers08= [1, 2, 3, 4, 5]
 numbers08.forEach(function(number) {
   console.log(number);
 });
@@ -17,13 +17,33 @@ map.set(3, 'three');
 map03.forEach(function(value, key) {
   console.log(key, value);
 });
-/*o/p
+o/p
 1 'one'
 2 'two'
 3 'three' 
- */
+ 
 //Iterating over a Set:
 const set = new Set([1, 2, 3, 4, 5]);
 set.forEach(function(value) {
   console.log(value);
+});*/
+
+
+//for each 
+/*  
+array.forEach(function(currentValue, index, array) {
+  // Your code here, where currentValue is the current element,
+  // index is the index of the current element, and array is the original array.
 });
+
+*/
+const numbers = [1, 2, 3, 4, 5];
+
+numbers.forEach(function(number, index) {
+  console.log(`Element at index ${index}: ${number}`);
+});
+const doubledNumbers = numbers.map(function(number) {
+  return number * 2;
+});
+
+console.log(doubledNumbers); // [2, 4, 6, 8, 10]
